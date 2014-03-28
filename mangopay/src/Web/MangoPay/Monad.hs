258f1f1ec63f9437d20b7ef73810c7373c1133a9
jpmoresmau <jp@moresmau.fr>
2014-03-28 15:19:05 +0100

    wrap HTTPException inside our Exception type

--- mangopay/src/Web/MangoPay/Monad.hs
+++ mangopay/src/Web/MangoPay/Monad.hs
@@ -194,8 +194,8 @@ mpReq req extractError addHeaders=do
           -- parse response as an error
           case fromJSON value of
             Success ise-> throw $ MpAppException $ extractError ise
-            _ -> throw err -- we can't even parse the error, throw the HTTP error
-    ) (\(_::ParseError)->throw err) -- the error body wasn't even json   
+            _ -> throw $ MpHttpException err -- we can't even parse the error, throw the HTTP error inside our error type
+    ) (\(_::ParseError)->throw $ MpHttpException err) -- the error body wasn't even json, throw the HTTP error inside our error type   
  
 -- | get a JSON response from a request to MangoPay
 -- MangoPay returns either a result, or an error
--- mangopay/src/Web/MangoPay/Types.hs
+++ mangopay/src/Web/MangoPay/Types.hs
@@ -15,6 +15,7 @@ import Data.Default
 import qualified Data.Text.Encoding as TE
 import qualified Data.ByteString.UTF8 as UTF8
 import Data.Maybe (listToMaybe)
+import Network.HTTP.Conduit (HttpException)
 
 -- | the MangoPay access point
 data AccessPoint = Sandbox | Production | Custom ByteString
@@ -85,6 +86,7 @@ toAccessToken  oa=AccessToken $ TE.encodeUtf8 $ T.concat [oaTokenType oa, " ",oa
 -- | an exception that a call to MangoPay may throw
 data MpException = JSONException String -- ^ JSON parsingError
   | MpAppException MpError -- ^ application exception
+  | MpHttpException HttpException -- ^ HTTP level exception
   deriving (Show,Typeable)
 
 -- | make our exception type a normal exception  
