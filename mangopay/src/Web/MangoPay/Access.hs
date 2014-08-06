f857d233ed4d1eedd4a9f8a85a973ebdc628fa7b
Felipe Lessa <felipe.lessa@gmail.com>
2014-08-05 21:22:49 -0300

    Style fixes.

--- mangopay/src/Web/MangoPay/Access.hs
+++ mangopay/src/Web/MangoPay/Access.hs
@@ -1,34 +1,35 @@
 {-# LANGUAGE FlexibleContexts, OverloadedStrings, ConstraintKinds #-}
--- | access methods for login, creating clients...
+-- | Obtaining credentials and access tokens.
 module Web.MangoPay.Access
-(
-createCredentialsSecret
-,oauthLogin
-)
-where
+  ( createCredentialsSecret
+  , oauthLogin
+  ) where
 
+import Control.Applicative ((<$>))
+import Data.Maybe (isNothing)
+import Data.Text
+import Network.HTTP.Conduit (applyBasicAuth)
 import Web.MangoPay.Monad
 import Web.MangoPay.Types
 
-import Data.Text
-
-import Network.HTTP.Conduit (applyBasicAuth)
-import Control.Monad (liftM)
-import qualified Network.HTTP.Types as HT
 import qualified Data.Text.Encoding as TE
-import Data.Maybe (isNothing)
+import qualified Network.HTTP.Types as HT
 
--- | populate the passphrase for our clientId IFF we don't have one
-createCredentialsSecret ::  (MPUsableMonad m) => MangoPayT m Credentials
-createCredentialsSecret =do
-        creds<- getCreds
-        if isNothing $ cClientSecret creds
-                then postExchange "/v2/clients" Nothing creds
-                else return creds
 
--- | login with given user name and password
--- returns the OAuth token that can be used to generate the opaque AccessToken and carries the expiration delay
-oauthLogin :: (MPUsableMonad m) => Text -> Text -> MangoPayT m OAuthToken
+-- | Populate the passphrase for our clientId IFF we don't have one.
+createCredentialsSecret :: MPUsableMonad m => MangoPayT m Credentials
+createCredentialsSecret = do
+  creds <- getCreds
+  if isNothing $ cClientSecret creds
+    then postExchange "/v2/clients" Nothing creds
+    else return creds
+
+-- | Login with given user name and password.  Returns the OAuth
+-- token that can be used to generate the opaque AccessToken and
+-- carries the expiration delay.
+oauthLogin :: MPUsableMonad m => Text -> Text -> MangoPayT m OAuthToken
 oauthLogin user pass = do
-        req<- liftM (applyBasicAuth (TE.encodeUtf8 user) (TE.encodeUtf8 pass)) $ getPostRequest "/v2/oauth/token" Nothing ([("grant_type",Just "client_credentials")]::HT.Query)
-        getJSONResponse req
+  let query = [("grant_type", Just "client_credentials")] :: HT.Query
+  req <- applyBasicAuth (TE.encodeUtf8 user) (TE.encodeUtf8 pass) <$>
+         getPostRequest "/v2/oauth/token" Nothing query
+  getJSONResponse req
